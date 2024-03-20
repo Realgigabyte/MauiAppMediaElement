68c3979b57344b0f40cd06cdf6f359b4073bbd57
realg <realg@DESKTOP-PJ3EFOI>
2024-03-20 18:45:13 +0000

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AbsoluteLayout absolutelayout = new AbsoluteLayout();
+            absolutelayout.BackgroundColor = Colors.Green;
+
+            MediaElement video= new MediaElement();
+            video.Source = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4";
+            video.ShouldShowPlaybackControls = false;
+            video.ShouldAutoPlay = true;
+
+            AbsoluteLayout.SetLayoutBounds(video, new Rect(0.5, 0.5, 1, 1));
+            AbsoluteLayout.SetLayoutFlags(video, Microsoft.Maui.Layouts.AbsoluteLayoutFlags.All);
+            absolutelayout.Add(video);
+            Content = absolutelayout;
+        }
+
+       
+    }
+
+}
